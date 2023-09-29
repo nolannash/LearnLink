c899b9f6a12371273f3af2c5675f1c19ad6ef4d9
nolan nash <n0lan.n4sh@gmail.com>
2023-09-29 01:03:09 -0700

    add classroom controller and routing

--- server/routes/classroom.js
+++ server/routes/classroom.js
@@ -1,15 +1,41 @@
-// routes/classroom.js
 const express = require('express');
 const router = express.Router();
-const classroomController = require('../controllers/classroomController');
+const passport = require('passport');
+const ClassroomController = require('../controllers/classroomController'); // Import the classroomController
 
 // Create a new classroom
-router.post('/create', classroomController.createClassroom);
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    ClassroomController.createClassroom(req, res);
+});
 
-// Add a student to a classroom
-router.post('/addStudent', classroomController.addStudentToClassroom);
+// Get all classrooms for a teacher
+router.get('/teacher/:teacherId', passport.authenticate('jwt', { session: false }), (req, res) => {
+    ClassroomController.getAllClassroomsForTeacher(req, res);
+});
 
-// Create an assignment in a classroom
-router.post('/createAssignment', classroomController.createAssignment);
+// Allow a student to join a classroom with a class key
+router.post('/join', (req, res) => {
+    ClassroomController.joinClassroom(req, res);
+});
+
+// Manually add a student to a classroom
+router.post('/add-student', passport.authenticate('jwt', { session: false }), (req, res) => {
+    ClassroomController.addStudentToClassroom(req, res);
+});
+
+// Add a lesson to a classroom
+router.post('/add-lesson', passport.authenticate('jwt', { session: false }), (req, res) => {
+    ClassroomController.addLessonToClassroom(req, res);
+});
+
+// Update a classroom's details
+router.patch('/:classroomId', passport.authenticate('jwt', { session: false }), (req, res) => {
+    ClassroomController.updateClassroomDetails(req, res);
+});
+
+// Delete a classroom
+router.delete('/:classroomId', passport.authenticate('jwt', { session: false }), (req, res) => {
+    ClassroomController.deleteClassroom(req, res);
+});
 
 module.exports = router;
