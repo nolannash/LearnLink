3e924fc1f9a7cbf9c61b0ddc34809bf1390ad28d
Angela Palaszewski <delarosa.ang@gmail.com>
2023-10-04 10:35:43 -0600

    dashboard sidebars

--- client/src/components/common/student/StudentDash.js
+++ client/src/components/common/student/StudentDash.js
@@ -2,7 +2,8 @@ import React, { useContext } from "react";
 
 import { Link } from "react-router-dom";
 import { AppContext } from "../../../AppContext";
-import { InlineIcon } from "@iconify/react";
+import { InlineIcon, Icon } from "@iconify/react";
+import divider from "../../../public/Divider.png";
 
 const StudentDash = () => {
   const { logoutStudent, student } = useContext(AppContext);
@@ -11,26 +12,65 @@ const StudentDash = () => {
     logoutStudent();
   };
   return (
-    <div className="grid grid-cols-6 min-h-[86vh]">
-      <div className="text-slate-900 row-span-full bg-emerald-100 rounded-xl m-6">
-        <div className="text-5xl font-bold italic text-green-900">
+    <div className="grid grid-cols-6 grid-rows-4 justify-between min-h-[86vh]">
+      <div className="text-slate-900 row-span-full bg-emerald-100 rounded-xl m-6 p-4 flex flex-col justify-between">
+        <div className="text-4xl font-bold italic text-green-900 text-center  ">
           learnlink
         </div>
-        <div>
-          <Link to="/">Home</Link>
-          <Link>Inbox</Link>
-          <Link>Classes</Link>
-          <Link>
+        <div className="text-xl font-medium ml-6">
+          <Link to="/" className="flex items-center gap-4 ">
+            <Icon icon="iconoir:home" />
+            Home
+          </Link>
+        </div>
+        <div className="text-xl font-medium ml-6">
+          <Link className="flex items-center gap-4 ">
+            <InlineIcon icon="octicon:inbox-24" />
+            Inbox
+          </Link>
+        </div>
+        <div className="text-xl font-medium ml-6">
+          <Link className="flex items-center gap-4 ">
+            <InlineIcon icon="healthicons:i-training-class" />
+            Classes
+          </Link>
+        </div>
+        <div className="text-xl font-medium ml-6">
+          <Link className="flex items-center gap-4 ">
             <InlineIcon icon="akar-icons:paper" />
             Progress
           </Link>
-          <Link>Files</Link>
-          <Link>Settings</Link>
-          <button onClick={handleLogout}>Log Out</button>
+        </div>
+        <div className="text-xl font-medium ml-6">
+          <Link className="flex items-center gap-4 ">
+            <InlineIcon icon="solar:folder-with-files-outline" />
+            Files
+          </Link>
+        </div>
+        <div className="text-xl font-medium ml-6">
+          <Link className=" flex items-center gap-2 ">
+            <InlineIcon icon="solar:settings-linear" />
+            Settings
+          </Link>
+        </div>
+        <div className="text-xl font-medium ml-6">
+          <button onClick={handleLogout} className="flex items-center gap-4">
+            <InlineIcon icon="clarity:logout-line" />
+            Log Out
+          </button>
+        </div>
+        <div className="self-center">
+          <button className="bg-amber-500 rounded-full h-20 w-20 p-2">
+            <Icon icon="mdi:robot-happy-outline" className="text-6xl" />
+          </button>
         </div>
       </div>
-      <div>
-        <h1>Welcome, {student?.name}</h1>
+
+      <div className="col-start-2 self-center justify-self-center">
+        <h1 className="text-2xl">Welcome, {student?.name}</h1>
+      </div>
+      <div className="col-start-3 col-span-3 self-center justify-self-center">
+        <img src={divider} alt="wavy divider" className="w-full" />
       </div>
     </div>
   );
--- client/src/components/common/teacher/TeacherDash.js
+++ client/src/components/common/teacher/TeacherDash.js
@@ -3,6 +3,7 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AppContext } from "../../../AppContext";
 import { InlineIcon, Icon } from "@iconify/react";
+import divider from "../../../public/Divider.png";
 
 function TeacherDash() {
   const { logoutTeacher, teacher } = useContext(AppContext);
@@ -12,55 +13,64 @@ function TeacherDash() {
   };
   return (
     <div className="grid grid-cols-6 grid-rows-4 justify-between min-h-[86vh]">
-      <div className="text-slate-900 row-span-full bg-emerald-100 rounded-xl m-6 p-4 flex flex-col">
-        <div className="text-4xl font-bold italic text-green-900 text-center">
+      <div className="text-slate-900 row-span-full bg-emerald-100 rounded-xl m-6 p-4 flex flex-col justify-between">
+        <div className="text-4xl font-bold italic text-green-900 text-center  ">
           learnlink
         </div>
-        <div className="text-xl font-medium flex-grow grid gap-4 place-items-center place-content-center">
-          <Link to="/" className="flex items-center gap2">
+        <div className="text-xl font-medium ml-6">
+          <Link to="/" className="flex items-center gap-4 ">
             <Icon icon="iconoir:home" />
             Home
           </Link>
-
-          <Link className="flex items-center gap2">
+        </div>
+        <div className="text-xl font-medium ml-6">
+          <Link className="flex items-center gap-4 ">
             <InlineIcon icon="octicon:inbox-24" />
             Inbox
           </Link>
-
-          <Link className="flex items-center gap2">
+        </div>
+        <div className="text-xl font-medium ml-6">
+          <Link className="flex items-center gap-4 ">
             <InlineIcon icon="healthicons:i-training-class" />
             Classes
           </Link>
-
-          <Link className="flex items-center gap-2">
+        </div>
+        <div className="text-xl font-medium ml-6">
+          <Link className="flex items-center gap-4 ">
             <InlineIcon icon="ph:student" />
             Students
           </Link>
-
-          <Link className="flex items-center gap-2">
+        </div>
+        <div className="text-xl font-medium ml-6">
+          <Link className="flex items-center gap-4 ">
             <InlineIcon icon="solar:folder-with-files-outline" />
             Files
           </Link>
-
-          <Link className=" flex items-center gap-2">
+        </div>
+        <div className="text-xl font-medium ml-6">
+          <Link className=" flex items-center gap-2 ">
             <InlineIcon icon="solar:settings-linear" />
             Settings
           </Link>
-
-          <button onClick={handleLogout} className="flex items-center gap-2">
+        </div>
+        <div className="text-xl font-medium ml-6">
+          <button onClick={handleLogout} className="flex items-center gap-4">
             <InlineIcon icon="clarity:logout-line" />
             Log Out
           </button>
         </div>
-        <div>
+        <div className="self-center">
           <button className="bg-amber-500 rounded-full h-20 w-20 p-2">
             <Icon icon="mdi:robot-happy-outline" className="text-6xl" />
           </button>
         </div>
       </div>
 
-      <div>
-        <h1 className="col-span-2 text-2xl">Welcome, {teacher?.name}</h1>
+      <div className="col-start-2 self-center justify-self-center">
+        <h1 className="text-2xl">Welcome, {teacher?.name}</h1>
+      </div>
+      <div className="col-start-3 col-span-3 self-center justify-self-center">
+        <img src={divider} alt="wavy divider" className="w-full" />
       </div>
       <div className="col-start-2 self-center justify-self-center">
         <Link
