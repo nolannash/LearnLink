e3f0559c2d952052f1003bd9b789903e1d4ae58d
nolan nash <n0lan.n4sh@gmail.com>
2023-10-03 21:10:55 -0700

    super sketch ai integration, needs functional api

--- server/controllers/Classroom/questionController.js
+++ server/controllers/Classroom/questionController.js
@@ -1,4 +1,5 @@
 const BaseQuestion = require("../models/questions/BaseQuestion"); // Make sure to adjust the path
+const { getAIResponse } = require('../../ai/gpt');
 
 exports.createQuestion = async (req, res) => {
   try {
@@ -15,5 +16,51 @@ exports.createQuestion = async (req, res) => {
       status: "fail",
       message: err,
     });
+  }
+};
+
+exports.requestHelp = async (req, res, questionId, studentInput, studentProfile) => {
+  try {
+      // Get the question based on questionId
+      const question = await BaseQuestion.findById(questionId);
+
+      if (!question) {
+          return res.status(404).json({ error: 'Question not found' });
+      }
+
+      // Customize the system message based on the student's profile and the app's goal
+      const systemMessage = `You are a patient teacher providing assistance to a student with ${studentProfile} to help with the following ${question.type} question: "${question.text}"`;
+
+      // Extract the maximum hint attempts from the question data
+      const maxHintAttempts = question.maxHintAttempts || 3; // Default to 3 if not specified
+
+      // Initialize variables for tracking attempts and messages
+      let currentAttempt = 0;
+      const messages = [];
+
+      // Loop through attempts
+      while (currentAttempt < maxHintAttempts - 1) {
+          // Create a message for the student's input (requesting a hint)
+          messages.push({ role: 'user', content: `Give me a hint for attempt ${currentAttempt + 1}` });
+
+          // Create a message for GPT's response (providing a hint)
+          const hint = await getAIResponse([...messages, { role: 'assistant', content: systemMessage }]);
+          messages.push({ role: 'assistant', content: hint });
+
+          // Increment the attempt
+          currentAttempt++;
+      }
+
+      // On the last attempt, reword the question
+      messages.push({ role: 'user', content: 'Please reword the question in a simpler manner.' });
+      messages.push({ role: 'assistant', content: await getAIResponse([...messages, { role: 'assistant', content: systemMessage }]) });
+
+      // Send the AI responses back to the student
+      res.json({ aiResponses: messages.map((msg) => msg.content) });
+  } catch (error) {
+      res.status(400).json({
+          status: 'fail',
+          message: error.message,
+      });
   }
 };
--- server/routes/student.js
+++ server/routes/student.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const StudentController = require('../controllers/studentControllerV2');
 const passport = require('passport');
+const { auth } = require('../middlewares/passport');
 
 const isAuthenticated = passport.authenticate('jwt', { session: false });
 
@@ -9,18 +10,27 @@ const isAuthenticated = passport.authenticate('jwt', { session: false });
 router.post('/signup', StudentController.signupStudent);
 router.post('/login', StudentController.signInStudent);
 
+
 router.get('/logout', (req, res) => {
 	req.logout();
 	res.redirect('/');
 });
 
 // Student-only Protected Routes
-router.patch('/:studentId/update', isAuthenticated, (req, res) => {
+router.patch('/:studentId/update', auth(['student']), (req, res) => {
 	StudentController.updateStudentProfile(req, res);
 });
 
-router.patch('/:studentId/update/password', isAuthenticated, (req, res) => {
+router.patch('/:studentId/update/password', auth(['student']), (req, res) => {
 	StudentController.updateStudentPassword(req, res);
+});
+
+router.post('/question/:questionId/request-help', auth(['student']), (req, res) => {
+    const { questionId } = req.params;
+    const { studentInput, studentProfile, maxAttempts } = req.body;
+
+    // Call the requestHelp function from questionController
+    questionController.requestHelp(req, res, questionId, studentInput, studentProfile, maxAttempts);
 });
 
 module.exports = router;
